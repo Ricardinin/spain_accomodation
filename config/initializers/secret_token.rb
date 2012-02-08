07c96cfbfcee419bd09c6e03221eb4aea54e32c7
Ricardo Vega <arvdosal@gmail.com>
2012-02-08 14:55:00 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpainAccomodation::Application.config.secret_token = '3dddafc8ffd8ee9aa3aa98289aaab0ba269b3af7458c8e8296f0700d302648cd0271a146c9fa88e57710f41c01b5821551de76e9bfe11a109f3c6e4cf9870a5c'
